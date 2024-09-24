d02d0dc8c56c61f28f9ec06bf6cdbdf4dbf1a040
OnlyDragon0403 <brunogendron6827@gmail.com>
2024-09-24 06:18:08 +0200

    "migrate 5.2 to 5.4"

--- Source/UnrealAlgorandPluginEditor.Target.cs
+++ Source/UnrealAlgorandPluginEditor.Target.cs
@@ -8,6 +8,7 @@ public class UnrealAlgorandPluginEditorTarget : TargetRules
 	public UnrealAlgorandPluginEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		ExtraModuleNames.AddRange( new string[] { "UnrealAlgorandPlugin" } );
 	}
 }
